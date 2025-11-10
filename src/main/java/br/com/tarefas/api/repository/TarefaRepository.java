7c847e2785d52241c4e74acb9f73e7073551647d
iKripto <yagonascimento0288@gmail.com>
2025-11-10 10:06:47 -0300

    ta feito

--- src/main/java/br/com/tarefas/api/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.tarefas.api.repository;
-
-import br.com.tarefas.api.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
--- src/main/java/com/example/sospets/config/WebConfig.java
+++ src/main/java/com/example/sospets/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.sospets.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Injeta a URL do frontend a partir das variáveis de ambiente ou usa localhost:3000 como padrão
+    @Value("${cors.origin.react:http://localhost:3000}")
+    private String reactAppUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite CORS para todos os endpoints ("/**")
-                .allowedOrigins("http://localhost:3000") // Adicione a porta do seu React app
+        registry.addMapping("/**") // Aplica a todos os endpoints
+                .allowedOrigins(reactAppUrl, "http://localhost:3000") // Permite o localhost e a URL de produção
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
